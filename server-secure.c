de77f25d9c4a8d14b60ee1a722a0e25a51baad4d
ToxicJuice23 <jujur2300@gmail.com>
2023-03-30 07:55:40 -0400

    Added better logging

--- server-secure.c
+++ server-secure.c
@@ -1,4 +1,13 @@
-#include "/home/jujur/coding/c/socket.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
 #include <stdlib.h>
 #include <openssl/crypto.h>
 #include <openssl/x509.h>
